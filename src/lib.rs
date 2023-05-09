f606db811fa39eb71bd4f066f3657bd1a975d895
Kaleb-Barrera <an26k@protonmail.com>
2023-05-09 15:44:22 -0600

    Updated leftpad function to handle negative numbers

--- src/lib.rs
+++ src/lib.rs
@@ -4,6 +4,9 @@
 extern crate napi_derive;
 
 #[napi]
-pub fn leftpad(original: String, len: u32, pat: Option<String>) -> String {
-  return pat.unwrap_or(String::from(" ")).repeat(len as usize) + &original;
+pub fn leftpad(input: String, len: i32, pat: Option<String>) -> String {
+  return pat
+    .unwrap_or(String::from(" "))
+    .repeat(if len.is_positive() { len as usize } else { 0 })
+    + &input;
 }
